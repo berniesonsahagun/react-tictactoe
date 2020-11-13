ca5eaeb5f8f718d6bfd056bdf1652635eda2348f
Bernieson Sahagun <berniesonsahagun@gmail.com>
2020-11-13 20:56:31 +0800

    refactored folder structure

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import Game from "components/Game";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+			<main>
+				<Game />
+			</main>
+		</div>
+	);
+}
+
+export default App;
